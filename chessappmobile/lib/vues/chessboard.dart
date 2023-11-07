b9dafa68db704c8c1974b4fb801c67533726a767
Mahdi Ellili <2003186@etu.cegepsth.qc.ca>
2023-11-06 20:40:40 -0500

    putting pawns in the board

--- chessappmobile/lib/vues/chessboard.dart
+++ chessappmobile/lib/vues/chessboard.dart
@@ -14,19 +14,45 @@ class GameBoard extends StatefulWidget{
 
 class _GameBoardState extends State<GameBoard> {
 
+  //Liste 2D pour les pièces
+  late List<List<ChessPiece?>> board;
+
+  //initialiser le board
+@override
+void initState(){
+  super.initState();
+  _initializeBoard();
+}
+
+void _initializeBoard(){
+    //initialiser le board avec des null
+  List<List<ChessPiece?>> newBoard = List.generate(8, (index) =>List.generate(8, (index)=>null));
+  for(int i = 0 ; i<8;i++) {
+    newBoard[1][i] = ChessPiece(PieceType: ChessPieceType.pawn, isWhite: false,
+        PathToImage: 'lib/images/pawn.png');
+    newBoard[6][i] = ChessPiece(PieceType: ChessPieceType.pawn, isWhite: true,
+        PathToImage: 'lib/images/pawn.png');
+  }
+
+  board = newBoard;
+}
   ChessPiece piecePawn = ChessPiece(
       PieceType:ChessPieceType.pawn, isWhite:false, PathToImage:'lib/images/pawn.png');
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.grey[600],
       body: GridView.builder (
         itemCount: 8*8,
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8), //Pour une grille 8 par 8
           itemBuilder: (context,  index) {
+
+          int row = index ~/8;
+          int col = index % 8;
             return Square(
             isWhite: bc.isWhite(index),
-            piece: piecePawn,
+            piece: board[row][col],
 
             );
           },
