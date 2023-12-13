28975a91f3792274072508183b5eddf21ad32b4b
Mahdi Ellili <2003186@etu.cegepsth.qc.ca>
2023-12-13 13:24:12 -0500

    fix dans la vue home

--- chessappmobile/lib/vues/login.dart
+++ chessappmobile/lib/vues/login.dart
@@ -1,28 +1,29 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../controleurs/providers/utilisateur_provider.dart';
 
-class Login extends StatelessWidget{
+class Login extends StatelessWidget {
   const Login();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Center(
-
-        child :ElevatedButton(onPressed: () async{
-          await Provider.of<UtilisateurProvider>(context, listen:false).loginAction();
-        },
-            child: const Text("Login")
-        ),),
+        ElevatedButton(
+          onPressed: () async {
+            await Provider.of<UtilisateurProvider>(context, listen: false)
+                .loginAction();
+          },
+          child: const Text("Login"),
+        ),
         Consumer<UtilisateurProvider>(
-          builder:(context, utilisateurProvider, child){
+          builder: (context, utilisateurProvider, child) {
             return Text(utilisateurProvider.errorMessage);
-          }
+          },
         ),
       ],
     );
